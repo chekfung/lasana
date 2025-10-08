dc7cd738ca37fad268ac6f013101af7c69dde203
honeydew01 <jumboberryhoneydew@gmail.com>
2025-10-08 15:50:46 -0500

    Updated script

--- code/io_space_analysis/run_lasana_multi.py
+++ code/io_space_analysis/run_lasana_multi.py
@@ -2,8 +2,12 @@ import os
 from pathlib import Path
 import subprocess
 import sys
+import time
+
+from datetime import datetime, timezone
 
 CONFIG_DIR = "io_space_analysis/configs"
+DATA_DIR = "../data"
 
 python_files = [
     "testbench_generation.py",             
@@ -44,12 +48,32 @@ def run_python_files(files, option=None, arg=None):
 
 if __name__ == "__main__":
     cmds = []
+    run_start_time = datetime.now(timezone.utc)
+    run_info_name = "run_info_" + run_start_time.strftime("%Y_%d_%m-%H_%M_%S") + ".txt"
+    run_info_file = Path(DATA_DIR) / run_info_name
+
+    print(f"Writing run information to {run_info_file}")
+    with open(run_info_file, "w") as f:
+        f.write(f'''Start simulations at {run_start_time.strftime("%H:%M:%S")} UTC\n\n''')
 
     config_dir = Path(CONFIG_DIR)
     for config in os.listdir(config_dir):
         config_name = Path(config).stem
+        config_start_time = datetime.now(timezone.utc)
+
+        with open(run_info_file, "a") as f:
+            f.write(f'''Processing configuration: "{config_name}" found in file: "{config}"\n''')
+            f.write(f'''Began processing at {config_start_time.strftime("%H:%M:%S")}\n''')
+
         print('\n\n---------------------------------------')
         print(f"Running the following files: [{python_files}]")
         print(f'''With current configuration: "{config_name}" defined in file "{config}"''')
 
-        # run_python_files(python_files, '--config', config_name)
+        run_python_files(python_files, '--config', config_name)
+
+        config_end_time = datetime.now(timezone.utc)
+        with open(run_info_file, "a") as f:
+            f.write(f'''Finished processing at {config_end_time.strftime("%H:%M:%S")}\n''')
+            f.write(f'''Total elapsed time: {config_start_time.second - config_end_time.second} seconds\n\n''')
+
+
